7f9017e1f67d9d04392531ca09593d11d678d72e
Mikdat Özal <tubacay34@gmail.com>
2023-02-22 22:31:37 +0300

    v3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -3,6 +3,7 @@ public class AtmApp
     public static void main(String[] args) {
         System.out.println("Yeni bir proje");
         System.out.println("version 2");
+        System.out.println("version 3");
 
     }
 }
